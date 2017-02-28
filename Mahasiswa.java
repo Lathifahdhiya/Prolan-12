9ed5177cf24508231ba4cd443c66099d1558cfd2
Lathifahdhiya <lathifahdhiya@gmail.com>
2017-02-28 19:09:46 +0700

    Delete Mahasiswa.java

--- Mahasiswa.java
+++
@@ -1,9 +0,0 @@
-public class Mahasiswa
-{
-	public static void main(String [] args)
-	{
-		System.out.println("Nama : Lathifah Dhiyaurrahmi \n");
-		System.out.println("NIM : 10115141 \n");
-		System.out.println("Alamat : Bandung \n");
-	}
-}
